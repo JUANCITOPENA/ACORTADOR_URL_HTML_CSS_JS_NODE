8cb9081b41a7c72250a8121e2101bbaa170ab84f
JUANCITO PEÑA V. <38921558+JUANCITOPENA@users.noreply.github.com>
2024-12-27 17:21:13 -0400

    Update script.js

--- public/script.js
+++ public/script.js
@@ -14,12 +14,13 @@ function shortenUrl() {
             }
             return response.json();
         }).then(data => {
+            console.log(data); // Asegúrate de ver los datos en la consola
             document.getElementById('shortUrl').innerText = data.shortUrl;
             document.getElementById('visitButton').style.display = 'inline';
             document.getElementById('copyButton').style.display = 'inline';
             document.getElementById('visitButton').setAttribute('onclick', `openInNewTab('${data.shortUrl}')`);
         }).catch(error => {
-            console.error('Error:', error);
+            console.error('Error:', error); // Muestra el error en la consola
             alert('Hubo un problema al acortar la URL.');
         });
     }
